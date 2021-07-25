dfc6a0c94c6130463d1c71b09972774ebf948e98
MohammadHossein <m.h.dolatabadi.a@gmail.com>
2021-07-26 01:10:01 +0430

    feat: Add title bar for each activity
    - Add title bar for course page
    - Add title bar for sub course page

--- app/src/main/java/ir/mk/learnx/adapters/SubCourseListAdapter.java
+++ app/src/main/java/ir/mk/learnx/adapters/SubCourseListAdapter.java
@@ -15,13 +15,9 @@ import java.util.ArrayList;
 
 import ir.mk.learnx.R;
 import ir.mk.learnx.model.SubCourseList;
-import ir.mk.learnx.quiz.QuestionActivity;
-import ir.mk.learnx.teach.LearnMovieActivity;
-<<<<<<< HEAD:app/src/main/java/ir/mk/learnx/adapters/SubCourseListAdapter.java
-=======
 import ir.mk.learnx.teach.LoadingLearn;
 import ir.mk.learnx.teach.SubCourseListActivity;
->>>>>>> 1427d70ed026203d075778230290aa6ef9f2385f:app/src/main/java/ir/mk/learnx/model/SubCourseListAdapter.java
+
 
 
 public class SubCourseListAdapter extends RecyclerView.Adapter<SubCourseListAdapter.ViewHolder> {
@@ -46,40 +42,12 @@ public class SubCourseListAdapter extends RecyclerView.Adapter<SubCourseListAdap
         holder.bind(current.getTitle(),current.getProgress(),current.getImageId());
         holder.itemView.setOnClickListener(v -> {
 //            Toast.makeText(v.getContext(), ""+current.getId(), Toast.LENGTH_SHORT).show();
-<<<<<<< HEAD:app/src/main/java/ir/mk/learnx/adapters/SubCourseListAdapter.java
-            int firstStepType = getFirstStepType(current.getLesson(), current.getCourseId(), current.getId());
-            int numberOfAllStep = getNumberOfAllStep(current.getLesson(), current.getCourseId(), current.getId());
-            switch (firstStepType){
-                case 0:
-                    Intent intent0 = new Intent(v.getContext(), LearnMovieActivity.class);
-                    intent0.putExtra("lesson",current.getLesson());
-                    intent0.putExtra("courseId",current.getCourseId());
-                    intent0.putExtra("subCourseId",current.getId());
-                    intent0.putExtra("step",1);
-                    intent0.putExtra("allStep",numberOfAllStep);
-                    v.getContext().startActivity(intent0);
-                    break;
-                case 1:
-                    Intent intent1 = new Intent(v.getContext(), QuestionActivity.class);
-                    intent1.putExtra("lesson",current.getLesson());
-                    intent1.putExtra("courseId",current.getCourseId());
-                    intent1.putExtra("subCourseId",current.getId());
-                    intent1.putExtra("step",1);
-                    intent1.putExtra("allStep",numberOfAllStep);
-                    v.getContext().startActivity(intent1);
-                    break;
-                default:
 
-                    break;
-            }
-
-=======
             Intent intent0 = new Intent((SubCourseListActivity)v.getContext(), LoadingLearn.class);
             intent0.putExtra("lesson",current.getLesson());
             intent0.putExtra("courseId",current.getCourseId());
             intent0.putExtra("subCourseId",current.getId());
             v.getContext().startActivity(intent0);
->>>>>>> 1427d70ed026203d075778230290aa6ef9f2385f:app/src/main/java/ir/mk/learnx/model/SubCourseListAdapter.java
         });
     }
 
